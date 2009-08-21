176ac097cca4e55f1ef58a2c9c09c1762942df81
Ed Gibbs <edward_gibbs@yahoo.com>
2009-08-20 20:19:09 -0700

    refactored duplicated reader to before method

--- ruby/data_munging/spec/reader_spec.rb
+++ ruby/data_munging/spec/reader_spec.rb
@@ -4,15 +4,17 @@ require File.join(File.dirname(__FILE__), '../lib/reader.rb')
 describe Reader do
 
   context "getting a file that needs only good lines parsed into array" do
+        
+    before(:each) do
+      @reader = Reader.new
+    end
   
     it "should return an array of 40 lines when passed in the weather file" do
-      reader = Reader.new
-      reader.get_lines('weather.dat').size.should == 40
+      @reader.get_lines('weather.dat').size.should == 40
     end
     
     it "should return an array of 41 lines when passed in the football file" do
-      reader = Reader.new
-      reader.get_lines('football.dat').size.should == 27
+      @reader.get_lines('football.dat').size.should == 27
     end
    
   end
