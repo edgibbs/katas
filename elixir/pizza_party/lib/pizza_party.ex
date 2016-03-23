45fddeb10450355c4994a9a834d364b38c26ac9b
Ed Gibbs <edward_gibbs@yahoo.com>
2016-03-22 19:55:39 -0700

    Passing tests for pizza party

--- elixir/pizza_party/lib/pizza_party.ex
+++ elixir/pizza_party/lib/pizza_party.ex
@@ -1,2 +1,13 @@
 defmodule PizzaParty do
+  defmodule CLI do
+    def main(_args) do
+    end
+
+    def slices_per_and_remainder(people, slices_per_pizza, number_of_pizzas) do
+      total_slices = slices_per_pizza * number_of_pizzas
+      slices_per_person = total_slices / people |> Float.floor |> round
+      remainder = rem(total_slices, slices_per_person)
+      {slices_per_person, remainder}
+    end
+  end
 end
--- elixir/pizza_party/test/pizza_party_test.exs
+++ elixir/pizza_party/test/pizza_party_test.exs
@@ -2,7 +2,15 @@ defmodule PizzaPartyTest do
   use ExUnit.Case
   doctest PizzaParty
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "divvy up pizzas exactly" do
+    {slices_per, remainder} = PizzaParty.CLI.slices_per_and_remainder(8, 8, 2)
+    assert slices_per == 2
+    assert remainder == 0
+  end
+
+  test "divvy up pizzas with leftovers" do
+    {slices_per, remainder} = PizzaParty.CLI.slices_per_and_remainder(2, 5, 1)
+    assert slices_per == 2
+    assert remainder == 1
   end
 end
