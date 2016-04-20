4578b7ef146b516b1ef04746b53cbc885654b5d8
Ed Gibbs <edward_gibbs@yahoo.com>
2016-04-19 22:19:47 -0700

    Finsh self checkout

--- elixir/self_checkout/lib/self_checkout.ex
+++ elixir/self_checkout/lib/self_checkout.ex
@@ -2,7 +2,14 @@ defmodule SelfCheckout do
   defmodule CLI do
     @tax_rate 0.055
     def main(_args) do
-
+      {item1_price, _} = IO.gets("Enter the price of item 1: ") |> String.strip |> Float.parse
+      {item1_quantity, _} = IO.gets("Enter the quantity of item 1: ") |> String.strip |> Float.parse
+      {item2_price, _} = IO.gets("Enter the price of item 2: ") |> String.strip |> Float.parse
+      {item2_quantity, _} = IO.gets("Enter the quantity of item 2: ") |> String.strip |> Float.parse
+      {item3_price, _} = IO.gets("Enter the price of item 3: ") |> String.strip |> Float.parse
+      {item3_quantity, _} = IO.gets("Enter the quantity of item 3: ") |> String.strip |> Float.parse
+      {subtotal, tax, total} = calculate_receipt([item1_price, item1_quantity], [item2_price, item2_quantity], [item3_price, item3_quantity])
+      IO.puts print_receipt(subtotal, tax, total)
     end
 
     def calculate_receipt(item1, item2, item3) do
@@ -10,7 +17,15 @@ defmodule SelfCheckout do
        |> + (List.first(item2) * List.last(item2))
        |> + (List.first(item3) * List.last(item3))
        tax = Float.round(subtotal * @tax_rate, 2)
-      [subtotal, tax, subtotal + tax]
+       {subtotal, tax, subtotal + tax}
+    end
+
+    defp print_receipt(subtotal, tax, total) do
+      """
+      Subtotal: $#{Float.to_string(subtotal, [decimals: 2])}
+      Tax: $#{Float.to_string(tax, [decimals: 2])}
+      Total: $#{Float.to_string(total, [decimals: 2])}
+      """
     end
   end
 end
--- elixir/self_checkout/test/self_checkout_test.exs
+++ elixir/self_checkout/test/self_checkout_test.exs
@@ -3,6 +3,6 @@ defmodule SelfCheckoutTest do
   doctest SelfCheckout
 
   test "prints a proper receipt" do
-    assert SelfCheckout.CLI.calculate_receipt([25, 2], [10, 1], [4, 1]) == [64.0, 3.52, 67.52]
+    assert SelfCheckout.CLI.calculate_receipt([25, 2], [10, 1], [4, 1]) == {64.0, 3.52, 67.52}
   end
 end
