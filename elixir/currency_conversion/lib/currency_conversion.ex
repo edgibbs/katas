63c39fe140b9b0733719d7dcdcb4436357a68ec7
Ed Gibbs <edward_gibbs@yahoo.com>
2016-04-28 21:52:41 -0700

    finish currency conversion

--- elixir/currency_conversion/lib/currency_conversion.ex
+++ elixir/currency_conversion/lib/currency_conversion.ex
@@ -1,2 +1,20 @@
 defmodule CurrencyConversion do
+  defmodule CLI do
+    def main(_args) do
+      {euros, _} = IO.gets("How many euros are you exchanging? ") |> String.strip |> Float.parse
+      {exchange_rate, _} = IO.gets("What is the exchange rate? ") |> String.strip |> Float.parse
+      dollars = dollars_to_euros(euros, exchange_rate)
+      IO.puts print_result(euros, exchange_rate, dollars)
+    end
+
+    def dollars_to_euros(euros, exchange_rate) do
+      euros * (exchange_rate / 100) |> Float.round(2)
+    end
+
+    defp print_result(euros, exchange_rate, dollars) do
+      """
+      #{euros} euros at the exchange_rate of #{exchange_rate} is #{dollars} U.S. dollars.
+      """
+    end
+  end
 end
--- elixir/currency_conversion/test/currency_conversion_test.exs
+++ elixir/currency_conversion/test/currency_conversion_test.exs
@@ -2,7 +2,7 @@ defmodule CurrencyConversionTest do
   use ExUnit.Case
   doctest CurrencyConversion
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "can convert to from dollars to euros" do
+    assert CurrencyConversion.CLI.dollars_to_euros(81, 137.51) == 111.38
   end
 end
