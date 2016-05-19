db1b5fb2461a88131a78c9587acb7a8b536607f6
Ed Gibbs <edward_gibbs@yahoo.com>
2016-05-18 21:54:23 -0700

    Complete car issues kata

--- elixir/car_issues/lib/car_issues.ex
+++ elixir/car_issues/lib/car_issues.ex
@@ -15,6 +15,39 @@ defmodule CarIssues do
     end
 
     defp clicking_noise do
+      is_clicking = IO.gets("Does the car make a clicking noise? ") |> String.strip |> String.downcase
+      if is_clicking == "y" do
+        IO.puts("Replace the battery.")
+      else
+        fail_to_start
+      end
+    end
+
+    defp fail_to_start do
+      is_not_starting = IO.gets("Does the car crank up but fail to start? ") |> String.strip |> String.downcase
+      if is_not_starting == "y" do
+        IO.puts("Check spark plug connections.")
+      else
+        engine_start_then_die
+      end
+    end
+
+    defp engine_start_then_die do
+      is_starting_then_die = IO.gets("Does the engine start and then die? ") |> String.strip |> String.downcase
+      if is_starting_then_die == "y" do
+        fuel_injection
+      else
+        IO.puts("Get it in for service.")
+      end
+    end
+
+    defp fuel_injection do
+      has_fuel_injection = IO.gets("Does your car have fuel injection? ") |> String.strip |> String.downcase
+      if has_fuel_injection == "y" do
+        IO.puts("Get it in for service.")
+      else
+        IO.puts("Check to ensure the choke is opening and closing.")
+      end
     end
   end
 end
--- elixir/car_issues/test/car_issues_test.exs
+++ elixir/car_issues/test/car_issues_test.exs
@@ -13,4 +13,46 @@ defmodule CarIssuesTest do
     Is the car silent when you turn the key? Are the battery terminals corroded? Clean terminals and try starting again.
     """
   end
+
+  test "when the terminals are good" do
+    assert capture_io([input: "y\nn"], car_issues) ==
+    """
+    Is the car silent when you turn the key? Are the battery terminals corroded? Replace cables and try again.
+    """
+  end
+
+  test "when the battery is dead" do
+    assert capture_io([input: "n\ny"], car_issues) ==
+    """
+    Is the car silent when you turn the key? Does the car make a clicking noise? Replace the battery.
+    """
+  end
+
+  test "when crank but fails to start" do
+    assert capture_io([input: "n\nn\ny"], car_issues) ==
+    """
+    Is the car silent when you turn the key? Does the car make a clicking noise? Does the car crank up but fail to start? Check spark plug connections.
+    """
+  end
+
+  test "when the engine starts and then dies" do
+    assert capture_io([input: "n\nn\nn\nn"], car_issues) ==
+    """
+    Is the car silent when you turn the key? Does the car make a clicking noise? Does the car crank up but fail to start? Does the engine start and then die? Get it in for service.
+    """
+  end
+
+  test "when the engine starts and then dies with fuel injection" do
+    assert capture_io([input: "n\nn\nn\ny\ny"], car_issues) ==
+    """
+    Is the car silent when you turn the key? Does the car make a clicking noise? Does the car crank up but fail to start? Does the engine start and then die? Does your car have fuel injection? Get it in for service.
+    """
+  end
+
+  test "when the engine starts and then dies without fuel injection" do
+    assert capture_io([input: "n\nn\nn\ny\nn"], car_issues) ==
+    """
+    Is the car silent when you turn the key? Does the car make a clicking noise? Does the car crank up but fail to start? Does the engine start and then die? Does your car have fuel injection? Check to ensure the choke is opening and closing.
+    """
+  end
 end
