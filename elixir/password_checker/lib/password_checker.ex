f79b130317174a0b5f7363c91d66e5ae091585ee
Ed Gibbs <edward_gibbs@yahoo.com>
2016-05-21 21:54:16 -0700

    Handle args from command line and complete tests

--- elixir/password_checker/lib/password_checker.ex
+++ elixir/password_checker/lib/password_checker.ex
@@ -1,24 +1,32 @@
 defmodule PasswordChecker do
   defmodule CLI do
-    def main(password) do
-      character_count = String.length(password)
-      has_number = Regex.match?(~r/\d+/, password)
-      has_letters = Regex.match?(~r/\w+/, password)
-      has_special_characters = Regex.match?(~r/[^\w\*]+/, password)
-      strength = password_validator(character_count, has_number, has_letters, has_special_characters)
-      case strength do
+    def main(args) do
+      {:ok, password} = Enum.fetch(args, 0)
+      password = password |> String.strip
+      case password_validator(password) do
         :very_weak -> IO.puts "The password '#{password}' is a very weak password."
+        :weak -> IO.puts "The password '#{password}' is a weak password."
+        :strong -> IO.puts "The password '#{password}' is a strong password."
+        :very_strong -> IO.puts "The password '#{password}' is a very strong password."
       end
     end
 
-    defp password_validator(character_count, has_number, has_letters, has_special_characters) do
+    defp password_validator(password) do
+      {character_count, has_number, has_letters, has_special_characters} = password_checks(password)
       cond do
         character_count > 7 && has_number && has_letters && has_special_characters -> :very_strong
         character_count > 7 && has_number && has_letters -> :strong
         character_count < 8 && !has_number && has_letters -> :weak
-        character_count < 8 && has_number && !has_letters -> :very_weak
         true -> :very_weak
       end
+    end
+
+    defp password_checks(password) do
+      character_count = String.length(password)
+      has_number = Regex.match?(~r/\d+/, password)
+      has_letters = Regex.match?(~r/\w+/, password)
+      has_special_characters = Regex.match?(~r/[^\w\*]+/, password)
+      {character_count, has_number, has_letters, has_special_characters}
     end
   end
 end
--- elixir/password_checker/test/password_checker_test.exs
+++ elixir/password_checker/test/password_checker_test.exs
@@ -4,7 +4,22 @@ defmodule PasswordCheckerTest do
   doctest PasswordChecker
 
   test "12345 is very weak" do
-    checker = fn -> PasswordChecker.CLI.main("12345") end
+    checker = fn -> PasswordChecker.CLI.main(["12345"]) end
     assert capture_io([], checker) == "The password '12345' is a very weak password.\n"
+  end
+
+  test "abcdef is weak" do
+    checker = fn -> PasswordChecker.CLI.main(["abcdef"]) end
+    assert capture_io([], checker) == "The password 'abcdef' is a weak password.\n"
+  end
+
+  test "abc123xyz is strong" do
+    checker = fn -> PasswordChecker.CLI.main(["abc123xyz"]) end
+    assert capture_io([], checker) == "The password 'abc123xyz' is a strong password.\n"
+  end
+
+  test "1337h@xor! is strong" do
+    checker = fn -> PasswordChecker.CLI.main(["1337h@xor!"]) end
+    assert capture_io([], checker) == "The password '1337h@xor!' is a very strong password.\n"
   end
 end
