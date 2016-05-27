853778d6a4fb995e787241b359331a165924e39e
Ed Gibbs <edward_gibbs@yahoo.com>
2016-05-26 22:46:08 -0700

    Passing tests for heart rate chart kata

--- elixir/heart_rate/lib/heart_rate.ex
+++ elixir/heart_rate/lib/heart_rate.ex
@@ -4,32 +4,44 @@ defmodule HeartRate do
       age = ask_age
       heart_rate = ask_heart_rate
       print_header
+      print_rows(55, age, heart_rate)
     end
 
     defp ask_age do
       age = IO.gets("What is your age? ") |> String.strip
       if Integer.parse(age) == :error do
         ask_age
+      else
+        {real_age, _} = Integer.parse(age)
+        real_age
       end
-      {real_age, _} = Integer.parse(age)
-      real_age
     end
 
     defp ask_heart_rate do
       heart_rate = IO.gets("What is your resting heart rate? ") |> String.strip
       if Integer.parse(heart_rate) == :error do
         ask_heart_rate
+      else
+        {real_heart_rate, _} = Integer.parse(heart_rate)
+        real_heart_rate
       end
-      {real_heart_rate, _} = Integer.parse(heart_rate)
-      heart_rate
     end
 
     defp print_header do
-      IO.puts """
+      IO.puts "\nIntensity    | Rate\n-------------|--------"
+    end
 
-      Intensity    | Rate
-      -------------|-----
-      """
+    defp print_rows(intensity, _age, _heart_rate) when intensity > 95 do
+      IO.puts("")
+    end
+
+    defp print_rows(intensity, age, heart_rate) do
+      IO.puts("#{intensity}%          | #{target_heart_rate(intensity, age, heart_rate)} bpm")
+      print_rows(intensity + 5, age, heart_rate)
+    end
+
+    defp target_heart_rate(intesity, age, heart_rate) do
+      ((220 - age) - heart_rate) * (intesity / 100) + heart_rate |> round
     end
   end
 end
--- elixir/heart_rate/test/heart_rate_test.exs
+++ elixir/heart_rate/test/heart_rate_test.exs
@@ -3,13 +3,42 @@ defmodule HeartRateTest do
   import ExUnit.CaptureIO
   doctest HeartRate
 
-  test "the truth" do
+  test "prints heart rate table" do
     heart_rate = fn -> HeartRate.CLI.main(nil) end
-    assert capture_io([input: "10\n80"], heart_rate) ==
+    assert capture_io([input: "22\n65\n"], heart_rate) ==
     """
     What is your age? What is your resting heart rate? 
     Intensity    | Rate
-    -------------|-----
+    -------------|--------
+    55%          | 138 bpm
+    60%          | 145 bpm
+    65%          | 151 bpm
+    70%          | 158 bpm
+    75%          | 165 bpm
+    80%          | 171 bpm
+    85%          | 178 bpm
+    90%          | 185 bpm
+    95%          | 191 bpm
+
+    """
+  end
+
+  test "handles invalid input for age and heart rate" do
+    heart_rate = fn -> HeartRate.CLI.main(nil) end
+    assert capture_io([input: "xxx\n22\nyyy\n65"], heart_rate) ==
+    """
+    What is your age? What is your age? What is your resting heart rate? What is your resting heart rate? 
+    Intensity    | Rate
+    -------------|--------
+    55%          | 138 bpm
+    60%          | 145 bpm
+    65%          | 151 bpm
+    70%          | 158 bpm
+    75%          | 165 bpm
+    80%          | 171 bpm
+    85%          | 178 bpm
+    90%          | 185 bpm
+    95%          | 191 bpm
 
     """
   end
