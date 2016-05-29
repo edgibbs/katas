86e139f7369f2e7fd9b7cfff19ebb5fcddcc642f
Ed Gibbs <edward_gibbs@yahoo.com>
2016-05-28 21:40:55 -0700

    Add first happy path test for guessing numbers kata

--- elixir/guess_numbers/lib/guess_numbers.ex
+++ elixir/guess_numbers/lib/guess_numbers.ex
@@ -1,2 +1,34 @@
 defmodule GuessNumbers do
+  defmodule CLI do
+    def main(args \\ [nil]) do
+      {:ok, not_random_number} = Enum.fetch(args, 0)
+      IO.puts "Let's play Guess the Number."
+      {difficulty_level, _} = IO.gets("Pick a difficulty level (1, 2, or 3): ") |> String.strip |> Integer.parse
+      secret_number = pick_random_number(difficulty_level, not_random_number)
+      {first_guess, _} = IO.gets("I have my number. What's your guess? ") |> String.strip |> Integer.parse
+      check_guess(first_guess, secret_number, 0)
+    end
+
+    defp check_guess(guess, secret_number, guesses) do
+      guesses = guesses + 1
+      cond do
+        guess == secret_number ->
+          IO.puts "You got it in #{guesses} guesses!"
+        guess > secret_number ->
+          {new_guess, _} = IO.gets("Too high. Guess again: ") |> String.strip |> Integer.parse
+          check_guess(new_guess, secret_number, guesses)
+        guess < secret_number ->
+          {new_guess, _} = IO.gets("Too low. Guess again: ") |> String.strip |> Integer.parse
+          check_guess(new_guess, secret_number, guesses)
+      end
+    end
+
+    defp pick_random_number(difficulty_level, not_random_number) do
+      if not_random_number do
+        not_random_number
+      else
+        :random.uniform(difficulty_level * 10)
+      end
+    end
+  end
 end
--- elixir/guess_numbers/test/guess_numbers_test.exs
+++ elixir/guess_numbers/test/guess_numbers_test.exs
@@ -1,8 +1,14 @@
 defmodule GuessNumbersTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest GuessNumbers
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "can play a simple guessing game" do
+    guess_numbers = fn -> GuessNumbers.CLI.main([2]) end
+    assert capture_io([input: "1\n1\n5\n2"], guess_numbers) ==
+      """
+      Let's play Guess the Number.
+      Pick a difficulty level (1, 2, or 3): I have my number. What's your guess? Too low. Guess again: Too high. Guess again: You got it in 3 guesses!
+      """
   end
 end
