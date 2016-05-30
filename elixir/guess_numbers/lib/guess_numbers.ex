f200d43ebe152f59b5c44a0dd5cfcbcdd678230b
Ed Gibbs <edward_gibbs@yahoo.com>
2016-05-29 20:02:21 -0700

    Pass flag for testing through the functions

--- elixir/guess_numbers/lib/guess_numbers.ex
+++ elixir/guess_numbers/lib/guess_numbers.ex
@@ -10,35 +10,35 @@ defmodule GuessNumbers do
       {difficulty_level, _} = IO.gets("Pick a difficulty level (1, 2, or 3): ") |> String.strip |> Integer.parse
       secret_number = pick_random_number(difficulty_level, not_random_number)
       {first_guess, _} = IO.gets("I have my number. What's your guess? ") |> String.strip |> Integer.parse
-      check_guess(first_guess, secret_number, 0)
+      check_guess(first_guess, secret_number, 0, not_random_number)
     end
 
-    defp check_guess(guess, secret_number, guesses) do
+    defp check_guess(guess, secret_number, guesses, not_random_number) do
       guesses = guesses + 1
       cond do
         guess == secret_number ->
           IO.puts "You got it in #{guesses} guesses!"
           play_again = IO.gets("Play again? ") |> String.strip
           if play_again == "y" do
-            if secret_number == 2 do
-              play_game(2) # pass along 2 for testing
+            if not_random_number do
+              play_game(not_random_number) # for testing
             else
               play_game(nil)
             end
           else
             IO.puts("All done.")
-            if secret_number == 2 do
-              # do nothing for testing
+            if not_random_number do
+              # for testing
             else
               System.halt(0)
             end
           end
         guess > secret_number ->
           {new_guess, _} = IO.gets("Too high. Guess again: ") |> String.strip |> Integer.parse
-          check_guess(new_guess, secret_number, guesses)
+          check_guess(new_guess, secret_number, guesses, not_random_number)
         guess < secret_number ->
           {new_guess, _} = IO.gets("Too low. Guess again: ") |> String.strip |> Integer.parse
-          check_guess(new_guess, secret_number, guesses)
+          check_guess(new_guess, secret_number, guesses, not_random_number)
       end
     end
 
