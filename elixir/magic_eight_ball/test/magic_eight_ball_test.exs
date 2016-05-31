ac879a493850019bd49637333dc66247fafb75ac
Ed Gibbs <edward_gibbs@yahoo.com>
2016-05-30 22:06:29 -0700

    Remove test description typo

--- elixir/magic_eight_ball/test/magic_eight_ball_test.exs
+++ elixir/magic_eight_ball/test/magic_eight_ball_test.exs
@@ -3,7 +3,7 @@ defmodule MagicEightBallTest do
   import ExUnit.CaptureIO
   doctest MagicEightBall
 
-  test "can ask the eight ball for an answer 2" do
+  test "can ask the eight ball for an answer" do
     magic_eight_ball = fn -> MagicEightBall.CLI.main(nil) end
     assert Regex.match?(~r/^What's your question?/, assert capture_io([import: ""], magic_eight_ball))
     assert Regex.match?(~r/(Yes\.|No\.|Maybe\.|Ask again later\.)$/, assert capture_io([import: ""], magic_eight_ball))
