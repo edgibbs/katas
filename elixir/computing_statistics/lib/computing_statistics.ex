4cf39da29429ccc8f2ea7059850f4c7929526219
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-02 20:11:17 -0700

    Complete stats kata, implement standard dev calculation

--- elixir/computing_statistics/lib/computing_statistics.ex
+++ elixir/computing_statistics/lib/computing_statistics.ex
@@ -10,13 +10,30 @@ defmodule ComputingStatistics do
     end
 
     defp ask_for_number("done", numbers) do
-      IO.puts "Numbers: "
+      IO.puts("Numbers: #{Enum.join(numbers, ", ")}")
+      IO.puts("The average is #{average(numbers)}.")
+      IO.puts("The minimum is #{Enum.min(numbers)}.")
+      IO.puts("The maximum is #{Enum.max(numbers)}.")
+      IO.puts("The standard deviation is #{standard_deviation(numbers)}.")
     end
 
     defp ask_for_number(number, numbers) do
       numbers = List.insert_at(numbers, -1, Integer.parse(number) |> elem(0))
       new_number = IO.gets("Enter a number: ") |> String.rstrip
       ask_for_number(new_number, numbers)
+    end
+
+    defp average(numbers) do
+      (Enum.sum(numbers) / Enum.count(numbers)) |> round
+    end
+
+    defp standard_deviation(numbers) do
+      count = Enum.count(numbers)
+      sum = Enum.sum(numbers)
+      sum_of_squares = Enum.reduce(numbers, 0, fn(number, accumulator) -> number * number + accumulator end)
+      difference = (count * sum_of_squares) - (sum * sum) |> abs
+      denominator = count * (count - 1)
+      :math.sqrt(difference / denominator) |> Float.round(2)
     end
   end
 end
--- elixir/computing_statistics/test/computing_statistics_test.exs
+++ elixir/computing_statistics/test/computing_statistics_test.exs
@@ -3,11 +3,15 @@ defmodule ComputingStatisticsTest do
   import ExUnit.CaptureIO
   doctest ComputingStatistics
 
-  test "" do
+  test "computes a bunch of stats for a series of numbers" do
     computing_statistics = fn -> ComputingStatistics.CLI.main(nil) end
     assert capture_io([input: "100\n200\n1000\n300\ndone\n"], computing_statistics)==
     """
-    Enter a number: Enter a number: Enter a number: Enter a number: Enter a number: Numbers: 
+    Enter a number: Enter a number: Enter a number: Enter a number: Enter a number: Numbers: 100, 200, 1000, 300
+    The average is 400.
+    The minimum is 100.
+    The maximum is 1000.
+    The standard deviation is 408.25.
     """
   end
 end
