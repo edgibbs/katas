116a81dd2db97eeda04e7b4dd8ff5e3e47f5fb58
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-03 22:08:54 -0700

    Handle input of for password generator

--- elixir/password_generator/lib/password_generator.ex
+++ elixir/password_generator/lib/password_generator.ex
@@ -1,2 +1,13 @@
 defmodule PasswordGenerator do
+  defmodule CLI do
+    def main(_args) do
+      {minimum, _} = IO.gets("What's the minimum length: ") |> String.strip |> Integer.parse
+      {number_of_special_characters, _} = IO.gets("How many special characters? ") |> String.strip |> Integer.parse
+      {numbers, _} = IO.gets("How many numbers? ") |> String.strip |> Integer.parse
+    end
+
+    defp available_characters do
+      ?a..?z |> Enum.map(&List.to_string([&1]))
+    end
+  end
 end
--- elixir/password_generator/test/password_generator_test.exs
+++ elixir/password_generator/test/password_generator_test.exs
@@ -1,8 +1,11 @@
 defmodule PasswordGeneratorTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest PasswordGenerator
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "generates random password" do
+    password_generator = fn -> PasswordGenerator.CLI.main(nil) end
+    assert capture_io([input: "8\n2\n2"], password_generator) ==
+    "What's the minimum length: How many special characters? How many numbers? "
   end
 end
