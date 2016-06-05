576c0b0e74ded1ca1f4b8c83f7d9f4b4b8431556
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-04 21:17:19 -0700

    Finish password generator kata

--- elixir/password_generator/lib/password_generator.ex
+++ elixir/password_generator/lib/password_generator.ex
@@ -4,10 +4,27 @@ defmodule PasswordGenerator do
       {minimum, _} = IO.gets("What's the minimum length: ") |> String.strip |> Integer.parse
       {number_of_special_characters, _} = IO.gets("How many special characters? ") |> String.strip |> Integer.parse
       {numbers, _} = IO.gets("How many numbers? ") |> String.strip |> Integer.parse
+      IO.puts "Your password is #{random_password(minimum, number_of_special_characters, numbers)}"
+    end
+
+    def random_password(minimum, number_of_special_characters, numbers) do
+      random_numbers = Enum.take_random(available_numbers, numbers)
+      random_special_characters = Enum.take_random(available_special_characters, number_of_special_characters)
+      random_characters = Enum.take_random(available_characters, (minimum - (number_of_special_characters + numbers)))
+      initial_password = random_numbers ++ random_special_characters ++ random_characters
+      Enum.shuffle(initial_password) |> Enum.join
     end
 
     defp available_characters do
       ?a..?z |> Enum.map(&List.to_string([&1]))
+    end
+
+    defp available_special_characters do
+      ~w[! @ # $ % ^ & * ( ) - _ + =]
+    end
+
+    defp available_numbers do
+      0..9
     end
   end
 end
--- elixir/password_generator/test/password_generator_test.exs
+++ elixir/password_generator/test/password_generator_test.exs
@@ -3,9 +3,15 @@ defmodule PasswordGeneratorTest do
   import ExUnit.CaptureIO
   doctest PasswordGenerator
 
-  test "generates random password" do
+  test "generates random password prompts and output" do
     password_generator = fn -> PasswordGenerator.CLI.main(nil) end
-    assert capture_io([input: "8\n2\n2"], password_generator) ==
-    "What's the minimum length: How many special characters? How many numbers? "
+    assert Regex.match?(~r/What's the minimum length: How many special characters\? How many numbers\? Your password is/, capture_io([input: "8\n2\n2"], password_generator))
+  end
+
+  test "generates a random password" do
+    password = PasswordGenerator.CLI.random_password(8, 3, 2)
+    assert String.length(password) == 8
+    assert String.codepoints(password) |> Enum.reject(fn(x) -> !String.match?(x, ~r/\d/) end) |> Enum.count == 2
+    assert String.codepoints(password) |> Enum.reject(fn(x) -> String.match?(x, ~r/([a-z]|[A-Z]|\d)/) end) |> Enum.count == 3
   end
 end
