d87c1bfd6263c9ef317fd1617758d0e12afcc4f8
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-13 21:22:24 -0700

    Passing all tests for parsing data file kata

--- elixir/parsing_data_file/lib/parsing_data_file.ex
+++ elixir/parsing_data_file/lib/parsing_data_file.ex
@@ -1,12 +1,17 @@
 defmodule ParsingDataFile do
   defmodule CLI do
     def main(_args) do
-      contents = File.read!("employees.csv")
-      IO.puts("Last     First     Salary\n ------------------------")
+      contents = File.read!("employees.csv") |> String.strip
+      IO.puts("Last     First    Salary\n------------------------")
       lines = String.split(contents, "\n")
-      Enum.map(lines, fn(line) ->
+      employees = Enum.map(lines, fn(line) ->
         employee = String.split(line, ",")
-        IO.puts "#{Enum.at(employee, 0)} #{Enum.at(employee, 1)} #{Enum.at(employee, 2)}\n"
+        %{first_name: Enum.at(employee, 1), last_name: Enum.at(employee, 0), salary: Enum.at(employee, 2)}
+      end)
+      longest_last_name = Enum.map(employees, fn(employee) -> String.length(employee.last_name) end) |> Enum.max
+      longest_first_name = Enum.map(employees, fn(employee) -> String.length(employee.first_name) end) |> Enum.max
+      Enum.map(employees, fn(employee) ->
+        IO.puts "#{String.ljust(employee.last_name, longest_last_name)} #{String.ljust(employee.first_name, longest_first_name)} #{employee.salary}"
       end)
     end
   end
--- elixir/parsing_data_file/test/parsing_data_file_test.exs
+++ elixir/parsing_data_file/test/parsing_data_file_test.exs
@@ -7,15 +7,15 @@ defmodule ParsingDataFileTest do
     parsing_data_file = fn -> ParsingDataFile.CLI.main(nil) end
     assert capture_io([input: ""], parsing_data_file) ==
     """
-    Last     First     Salary
-    -------------------------
-    Ling     Mai       55900
-    Johnson  Jim       56500
-    Jones    Aaron     46000
-    Jones    Chris     34500
-    Swift    Geoffrey  14200
-    Xiong    Fong      65000
-    Zarnecki Sabrina   51500
+    Last     First    Salary
+    ------------------------
+    Ling     Mai      55900
+    Johnson  Jim      56500
+    Jones    Aaron    46000
+    Jones    Chris    34500
+    Swift    Geoffrey 14200
+    Xiong    Fong     65000
+    Zarnecki Sabrina  51500
     """
   end
 end
