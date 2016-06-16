ddf73750ff617a332d918f7602be5d5931cee2f2
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-15 21:22:47 -0700

    Testing input for website generator kata

--- elixir/website_generator/lib/website_generator.ex
+++ elixir/website_generator/lib/website_generator.ex
@@ -1,2 +1,11 @@
 defmodule WebsiteGenerator do
+  defmodule CLI do
+    def main(_args) do
+      site_name = IO.gets("Site name: ") |> String.strip
+      author = IO.gets("Author: ") |> String.strip
+      javascript_folder = IO.gets("Do you want a folder for Javascript? ") |> String.strip
+      css_folder = IO.gets("Do you want a folder for CSS? ") |> String.strip
+
+    end
+  end
 end
--- elixir/website_generator/test/website_generator_test.exs
+++ elixir/website_generator/test/website_generator_test.exs
@@ -1,8 +1,11 @@
 defmodule WebsiteGeneratorTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest WebsiteGenerator
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "templates a website" do
+    website_generator = fn -> WebsiteGenerator.CLI.main(nil) end
+    assert capture_io([input: "awesomeco\nMax Power\ny\ny"], website_generator) ==
+    "Site name: Author: Do you want a folder for Javascript? Do you want a folder for CSS? "
   end
 end
