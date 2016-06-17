889b05a8eb1ba659d926d866be5a503b7cfab518
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-16 20:42:19 -0700

    Finish website generator kata

--- elixir/website_generator/lib/website_generator.ex
+++ elixir/website_generator/lib/website_generator.ex
@@ -5,7 +5,34 @@ defmodule WebsiteGenerator do
       author = IO.gets("Author: ") |> String.strip
       javascript_folder = IO.gets("Do you want a folder for Javascript? ") |> String.strip
       css_folder = IO.gets("Do you want a folder for CSS? ") |> String.strip
+      create_folder(site_name, "y")
+      create_index_file(site_name, author)
+      create_folder(site_name <> "/js", javascript_folder)
+      create_folder(site_name <> "/css", css_folder)
+    end
 
+    defp create_folder(site_name, "y") do
+      File.mkdir(site_name)
+      IO.puts("Created ./#{site_name}")
+    end
+
+    defp create_folder(site_name, no) do end
+
+    defp create_index_file(site_name, author) do
+      {:ok, index_html} = File.open("#{site_name}/index.html", [:write])
+      IO.binwrite(index_html,
+      """
+      <html>
+      <head>
+      <title>#{site_name}</title>
+      <meta name="Author" content="#{author}">
+      </head>
+      <body>
+      </body>
+      </html>
+      """
+      )
+      IO.puts("Created ./#{site_name}/index.html")
     end
   end
 end
--- elixir/website_generator/test/website_generator_test.exs
+++ elixir/website_generator/test/website_generator_test.exs
@@ -3,9 +3,58 @@ defmodule WebsiteGeneratorTest do
   import ExUnit.CaptureIO
   doctest WebsiteGenerator
 
+  def website_generator do
+    fn -> WebsiteGenerator.CLI.main(nil) end
+  end
+
+  setup do
+    on_exit fn ->
+      File.rm_rf("awesomeco")
+    end
+  end
+
   test "templates a website" do
-    website_generator = fn -> WebsiteGenerator.CLI.main(nil) end
     assert capture_io([input: "awesomeco\nMax Power\ny\ny"], website_generator) ==
-    "Site name: Author: Do you want a folder for Javascript? Do you want a folder for CSS? "
+    "Site name: Author: Do you want a folder for Javascript? Do you want a folder for CSS? Created ./awesomeco\nCreated ./awesomeco/index.html\nCreated ./awesomeco/js\nCreated ./awesomeco/css\n"
+  end
+
+  test "creates a directory" do
+    capture_io([input: "awesomeco\nMax Power\ny\ny"], website_generator)
+    assert File.exists?("awesomeco")
+  end
+
+  test "creates a file with the authors name" do
+    capture_io([input: "awesomeco\nMax Power\ny\ny"], website_generator)
+    assert File.read!("awesomeco/index.html") ==
+    """
+    <html>
+    <head>
+    <title>awesomeco</title>
+    <meta name="Author" content="Max Power">
+    </head>
+    <body>
+    </body>
+    </html>
+    """
+  end
+
+  test "creates a javascript folder" do
+    capture_io([input: "awesomeco\nMax Power\ny\ny"], website_generator)
+    assert File.exists?("awesomeco/js")
+  end
+
+  test "doesn't create a javascript folder if not asked for" do
+    capture_io([input: "awesomeco\nMax Power\nn\ny"], website_generator)
+    refute File.exists?("awesomeco/js")
+  end
+
+  test "creates a css folder" do
+    capture_io([input: "awesomeco\nMax Power\ny\ny"], website_generator)
+    assert File.exists?("awesomeco/css")
+  end
+
+  test "doesn't create a css folder if not asked for" do
+    capture_io([input: "awesomeco\nMax Power\ny\nn"], website_generator)
+    refute File.exists?("awesomeco/css")
   end
 end
