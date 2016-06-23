2712550da133cfcc557dd4fe60feece3b18a96cc
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-22 20:48:38 -0700

    Complete word finder kata

--- elixir/word_finder/lib/word_finder.ex
+++ elixir/word_finder/lib/word_finder.ex
@@ -1,2 +1,11 @@
 defmodule WordFinder do
+  defmodule CLI do
+    def main(_args) do
+      file_name = IO.gets("What do you want the file name to be? ") |> String.strip
+      contents = File.read!("sample.txt") #|> String.strip
+      replaced_contents = String.replace(contents, "utilize", "use")
+      {:ok, file} = File.open(file_name, [:write])
+      IO.binwrite(file, replaced_contents)
+    end
+  end
 end
--- elixir/word_finder/test/word_finder_test.exs
+++ elixir/word_finder/test/word_finder_test.exs
@@ -1,8 +1,19 @@
 defmodule WordFinderTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest WordFinder
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    on_exit fn ->
+      File.rm("result.txt")
+    end
+  end
+
+  test "writes a replace file" do
+    word_finder = fn -> WordFinder.CLI.main(nil) end
+    assert capture_io([input: "result.txt\n"], word_finder) ==
+    "What do you want the file name to be? "
+    assert File.read!("result.txt") ==
+    "One should never use the word \"use\" in writing. Use \"use\" instead.\n"
   end
 end
