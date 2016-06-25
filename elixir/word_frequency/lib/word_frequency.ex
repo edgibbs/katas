72e0367a2cbd11ffee18455cba652f382ff1849a
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-24 21:49:12 -0700

    Finish word frequency kata

--- elixir/word_frequency/lib/word_frequency.ex
+++ elixir/word_frequency/lib/word_frequency.ex
@@ -1,2 +1,25 @@
 defmodule WordFrequency do
+  defmodule CLI do
+    def main(_args) do
+      File.read!("words.txt")
+      |> String.strip
+      |> String.split(" ")
+      |> Enum.sort
+      |> Enum.chunk_by(&(&1))
+      |> Enum.sort(&(Enum.count(&1) > Enum.count(&2)))
+      |> Enum.each(fn(word_list) -> print_line(word_list) end)
+    end
+
+    defp print_line(word_list) do
+      IO.puts "#{List.first(word_list)}: #{print_asterisks(Enum.count(word_list), "") }"
+    end
+
+    defp print_asterisks(0, asterisk_line) do
+      asterisk_line
+    end
+
+    defp print_asterisks(number, asterisk_line) do
+      print_asterisks(number - 1, asterisk_line <> "*")
+    end
+  end
 end
--- elixir/word_frequency/test/word_frequency_test.exs
+++ elixir/word_frequency/test/word_frequency_test.exs
@@ -1,8 +1,15 @@
 defmodule WordFrequencyTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest WordFrequency
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "prints out sorted word frequency" do
+    word_frequency = fn -> WordFrequency.CLI.main(nil) end
+    assert capture_io([input: ""], word_frequency) ==
+    """
+    badger: *******
+    mushroom: **
+    snake: *
+    """
   end
 end
