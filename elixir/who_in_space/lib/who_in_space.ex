e44b983b9039b4b1dabb632a33839fef733ec88e
Ed Gibbs <edward_gibbs@yahoo.com>
2016-06-25 22:16:55 -0700

    Complete kata for who's in space

--- elixir/who_in_space/lib/who_in_space.ex
+++ elixir/who_in_space/lib/who_in_space.ex
@@ -1,17 +1,17 @@
 defmodule WhoInSpace do
   defmodule CLI do
-    def main(_args) do
-      HTTPotion.start
-      response = HTTPotion.get "http://api.open-notify.org/astros.json"
+    def main(args) do
+      http_client = List.first(args) || HTTPotion
+      http_client.start
+      response = http_client.get("http://api.open-notify.org/astros.json")
       json_result = Poison.Parser.parse!(response.body)
       IO.puts "Name                | Craft"
       IO.puts "--------------------|------"
       astronauts = Enum.map(json_result["people"], fn(person) ->
         %{name: person["name"], craft: person["craft"]}
       end)
-      longest_name = Enum.map(astronauts, fn(astronaut) -> String.length(astronaut.name) end) |> Enum.max
       Enum.map(astronauts, fn(astronaut) ->
-        IO.puts "#{String.ljust(astronaut.name, longest_name)} | #{astronaut.craft}"
+        IO.puts "#{String.ljust(astronaut.name, 19)} | #{astronaut.craft}"
       end)
     end
   end
--- elixir/who_in_space/test/who_in_space_test.exs
+++ elixir/who_in_space/test/who_in_space_test.exs
@@ -4,14 +4,33 @@ defmodule WhoInSpaceTest do
   doctest WhoInSpace
 
   test "the truth" do
-    who_in_space = fn -> WhoInSpace.CLI.main(nil) end
+    who_in_space = fn -> WhoInSpace.CLI.main([FakeHttpClient]) end
     assert capture_io([input: ""], who_in_space) ==
     """
     Name                | Craft
     --------------------|------
-    Gennady Padalka     | ISS
-    Mikhail Kornienko   | ISS
-    Scott Kelly         | ISS
+    Hila Yehuda         | ISS
+    Ivan Terrible       | ISS
+    """
+  end
+end
+
+defmodule FakeHttpClient do
+  def start do
+  end
+
+  def get(url) do
+    %{body: people}
+  end
+
+  defp people do
+    """
+    {
+    "people":
+      [ { "name": "Hila Yehuda", "craft": "ISS" },
+        { "name": "Ivan Terrible", "craft": "ISS" }
+        ]
+    }
     """
   end
 end
