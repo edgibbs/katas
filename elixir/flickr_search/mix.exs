af168da8ae86c2c0e3767186fafcfdd9555c413a
Ed Gibbs <edward_gibbs@yahoo.com>
2016-07-02 22:05:31 -0700

    Add call to raw Flickr api

--- elixir/flickr_search/mix.exs
+++ elixir/flickr_search/mix.exs
@@ -33,6 +33,8 @@ defmodule FlickrSearch.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:poison, "~> 2.0"},
+     {:httpotion, "~> 3.0.0"},
      {:cowboy, "~> 1.0"}]
   end
 end
--- elixir/flickr_search/web/controllers/search_controller.ex
+++ elixir/flickr_search/web/controllers/search_controller.ex
@@ -1,7 +1,16 @@
 defmodule FlickrSearch.SearchController do
   use FlickrSearch.Web, :controller
+  require Logger
 
   def index(conn, _params) do
+    photos = fetch_photos
+    Logger.debug photos
     render conn, "index.html"
+  end
+
+  defp fetch_photos do
+    http_client = HTTPotion
+    http_client.start
+    http_client.get("https://api.flickr.com/services/feeds/photos_public.gne?tag=cat").body
   end
 end
